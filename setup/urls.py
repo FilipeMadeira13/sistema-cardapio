cc5df4aa9d8d0c57400da0217edb5f565a55a078
FilipeMadeira13 <cfilipemadeira@gmail.com>
2026-08-13 12:31:56 -0300

    Isola as URLs do app

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from cardapio.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index),
+    path("", include('cardapio.urls')),
 ]
