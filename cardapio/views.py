b60514fddee649cb84fbe7eba9bb87dda1ea04b1
FilipeMadeira13 <cfilipemadeira@gmail.com>
2026-08-13 14:56:00 -0300

    Testando index.html

--- cardapio/views.py
+++ cardapio/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Cardápio Online</h1><p>Bem-vindo ao cardápio online</p>")
+    return render(request, 'index.html')
