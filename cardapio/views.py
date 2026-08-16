99f0d3e3277d52504cf496bb7568b97ce63d427d
FilipeMadeira13 <cfilipemadeira@gmail.com>
2026-08-16 17:43:07 -0300

    Deixa o cardápio com valores dinâmicos

--- cardapio/views.py
+++ cardapio/views.py
@@ -6,4 +6,16 @@ def index(request):
 
 
 def menu(request):
-    return render(request, "cardapio/menu.html")
+    dados = {
+        1: {
+            "nome": "Frango Assado",
+            "descricao": "O melhor frango assado da cidade.",
+            "preco": 27.00,
+        },
+        2: {
+            "nome": "Prato Feito",
+            "descricao": "Delicioso prato com opções variádas.",
+            "preco": 19.00,
+        },
+    }
+    return render(request, "cardapio/menu.html", {"pratos": dados})
